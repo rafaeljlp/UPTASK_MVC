026dee7604c44fdf403f335fdbbe40d467df8f61
rafaeljlp <108440230+rafaeljlp@users.noreply.github.com>
2022-09-13 19:41:57 -0500

    13/09/2022

--- classes/Email.php
+++ classes/Email.php
@@ -35,7 +35,7 @@ class Email {
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en 
             UpTask, solo debes confirmarla en el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar" .
+        $contenido .= "<p>Presiona aquí: <a href='https://dry-plateau-73245.herokuapp.com/confirmar" .
             "?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
         $contenido .= '</html>';
@@ -52,8 +52,8 @@ class Email {
         $mail->Host = 'smtp.mailtrap.io';
         $mail->SMTPAuth = true;
         $mail->Port = 2525;
-        $mail->Username = '0acaf3d38550b4';
-        $mail->Password = '0b59277437f7b4';
+        $mail->Username = '69808259e7bb1a';
+        $mail->Password = '557e3f309c14c1';
 
         $mail->setFrom('cuentas@uptask.com');
         $mail->addAddress('cuentas@uptask.com', 'uptask.com');
@@ -65,7 +65,7 @@ class Email {
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Parece que has olvidado tu
         password, sigue el siguiente enlace para recuperarlo</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/reestablecer?token=" . 
+        $contenido .= "<p>Presiona aquí: <a href='https://dry-plateau-73245.herokuapp.com/reestablecer?token=" . 
         $this->token . "'>Reestablecer Password</a></p>";
         $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
         $contenido .= '</html>';
