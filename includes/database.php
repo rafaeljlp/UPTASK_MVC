0d9b513bed6e9032ca6dcc1b83e6773a84dbe6e5
rafaeljlp <108440230+rafaeljlp@users.noreply.github.com>
2023-01-07 20:58:44 -0600

    07/01/2023 database 2

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,13 @@
 <?php
 
-// $db = mysqli_connect(
-//     $_ENV['DB_HOST'], 
-//     $_ENV['DB_USER'],
-//     $_ENV['DB_PASS'] ?? '',
-//     $_ENV['DB_BD']
-// );
+$db = mysqli_connect(
+    $_ENV['DB_HOST'], 
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'] ?? '',
+    $_ENV['DB_BD']
+);
 
-$db = mysqli_connect('ralphtechnology.com', 'u347393537_root', 'Admin1001', 'u347393537_uptask_mvc');
+// $db = mysqli_connect('ralphtechnology.com', 'u347393537_root', 'Admin1001', 'u347393537_uptask_mvc');
 
 $db->set_charset('utf8');
 
